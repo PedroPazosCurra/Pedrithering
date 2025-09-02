7dedc504f6ca7ea349263e1a3b0f8d1262abba63
PedroPazosCurra <pedropazos11@gmail.com>
2025-09-02 02:02:34 +0200

    Rebotes ahora funcionan de verdad y movimiento mucho más fluido

--- src/interfaz.py
+++ src/interfaz.py
@@ -5,19 +5,27 @@
 """
 
 from PyQt6 import QtGui, QtWidgets
-from PyQt6.QtCore import Qt, QPoint, QSize, QTimer
+from PyQt6.QtCore import Qt, QPoint, QSize, QTimer, QCoreApplication
 from PIL import ImageQt
 from gestion_recursos import importar_imagen_a_QImage
 import sys
-from threading import Thread
-from time import sleep
 from datetime import datetime
 import math
 
 
+
+LIMITE_SUPERIOR_ANCHO = 1920 # TODO: Tomar dimensiones de la pantalla en lugar de hardcodear esto
+LIMITE_SUPERIOR_ALTO = 1080
+LIMITE_INFERIOR_ANCHO = 0
+LIMITE_INFERIOR_ALTO = 0
+ANCHO_VENTANA = 100 # TODO: Tomar dimensiones del png en lugar de hardcodear esto
+ALTO_VENTANA = 100
 FILENAME_MASCARA_VENTANA = "mascara_ventana.png"
-ANCHO_VENTANA = 600
-ALTO_VENTANA = 400
+
+# Debug
+#LIMITE_SUPERIOR_ANCHO = 600
+#LIMITE_SUPERIOR_ALTO = 400
+
 
 
 class Aviso_Personalizado(QtWidgets.QMessageBox):
@@ -38,20 +46,54 @@ class Aviso_Personalizado(QtWidgets.QMessageBox):
             self.show()
 
 
+class Ventana_Bocadillo_Mensaje(QtWidgets.QWidget):
+    """Bocadillo que soltará la ventana principal en determinadas ocasiones"""
+
+    def __init__(self, mensaje : str = "Error desconocido hasta que se demuestre lo contrario"):
+        super().__init__()
+        
+        self.setWindowFlags(Qt.WindowType.FramelessWindowHint) # Ventana sin bordes
+        self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground) # Fondo transparente (permite alfa en el PNG)
+
+        self.mensaje = mensaje
+
+        qimage_mascara = importar_imagen_a_QImage("aviso.png")
+        self.pixmap = QtGui.QPixmap.fromImage(qimage_mascara)
+
+        # Error cargando pixmap
+        if self.pixmap.isNull():
+
+            self.aviso = Aviso_Personalizado(titulo="Error cargando imagen", 
+                                             mensaje=   f"No se pudo crear QPixmap.\n"
+                                                        f"Formatos soportados: "
+                                                        f"{', '.join(fmt.data().decode() for fmt in QtGui.QImageReader.supportedImageFormats())}")
+            self.aviso.ensenha()
+            return
+
+
+    def ensenha(self):
+            """Aparece esta ventana en las coordenadas de la ventana principal"""
+
+            #self.setText(self.mensaje)
+            self.show()
+
+
 class Ventana_Custom(QtWidgets.QWidget):
     """Clase de ventana personalizada con forma no ortodoxa"""
 
-    def __init__(self):
+    def __init__(self, ancho_pantalla, alto_pantalla):
         super().__init__()
 
         # DEBUG Aviso personalizado
         #self.aviso = Aviso_Personalizado()
         #self.aviso.ensenha()
 
-        self.ultima_medida_posicion_inercia = (self.x(),self.y())
+        self.ultima_medida_posicion_inercia = (self.x(), self.y())
         self.inercia = 0
         self.direccion_movimiento = 0
         self.agarrado = False
+
+        self.bocadillo = Ventana_Bocadillo_Mensaje()
         
         self.setWindowFlags(Qt.WindowType.FramelessWindowHint) # Ventana sin bordes
         self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground) # Fondo transparente (permite alfa en el PNG)
@@ -70,17 +112,17 @@ class Ventana_Custom(QtWidgets.QWidget):
             return
 
         
-        self.resize(ANCHO_VENTANA, ALTO_VENTANA) # Redimensionar ventana a la imagen
+        self.resize(LIMITE_SUPERIOR_ANCHO - LIMITE_INFERIOR_ANCHO, LIMITE_SUPERIOR_ANCHO - LIMITE_INFERIOR_ALTO) # Redimensionar ventana a la imagen
 
         # Animacion de movimiento
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.loop_movimiento)
-        self.timer.start(50)
+        self.timer.start(20)
 
         # Calculo de inercia
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.loop_calcular_inercia)
-        self.timer.start(50)
+        self.timer.start(20)
 
 
     def paintEvent(self, event):
@@ -125,14 +167,28 @@ class Ventana_Custom(QtWidgets.QWidget):
 
         if not self.agarrado:
 
-            valor_bamboleo = self.inercia <= 1 and (3*math.sin(round(datetime.now().microsecond / 150000, 2)))
+            valor_bamboleo = round(self.inercia <= 1 and (2*math.sin(round(datetime.now().microsecond / 150000, 2))))
 
-            nuevo_x = self.x() + round(math.sin(self.direccion_movimiento) * self.inercia/5)
-            nuevo_y = self.y() + round(math.cos(self.direccion_movimiento) * self.inercia/5 + valor_bamboleo)
+            escalar_vector_movimiento_x = round(math.sin(self.direccion_movimiento) * self.inercia)
+            escalar_vector_movimiento_y = round(math.cos(self.direccion_movimiento) * self.inercia)
 
-            # Confino las coordenadas a la ventana para que no se salga
-            x = max(min(nuevo_x, ANCHO_VENTANA), 0)
-            y = max(min(nuevo_y, ALTO_VENTANA), 0)
+            nuevo_x = self.x() + escalar_vector_movimiento_x
+            nuevo_y = self.y() + escalar_vector_movimiento_y + valor_bamboleo
+
+            # Rebote en esquinas
+            if nuevo_x <= LIMITE_INFERIOR_ANCHO or nuevo_x >= LIMITE_SUPERIOR_ANCHO - ANCHO_VENTANA:
+                nuevo_x = self.x() - escalar_vector_movimiento_x
+                # TODO: Cambio de png cuando rebote
+                #self.bocadillo.ensenha()
+            elif  nuevo_y <= LIMITE_INFERIOR_ALTO or nuevo_y >= LIMITE_SUPERIOR_ALTO - ALTO_VENTANA:
+                nuevo_y = self.y() - escalar_vector_movimiento_y + valor_bamboleo
+                # TODO: Cambio de png cuando rebote
+                #self.bocadillo.ensenha()
+
+            self.direccion_movimiento = math.atan2(nuevo_x - self.x(), nuevo_y - self.y())
+
+            x = max(min(nuevo_x, LIMITE_SUPERIOR_ANCHO-ANCHO_VENTANA), LIMITE_INFERIOR_ANCHO+1)
+            y = max(min(nuevo_y, LIMITE_SUPERIOR_ALTO-ALTO_VENTANA), LIMITE_INFERIOR_ALTO+1)
 
             print(str(x) + " , " + str(y) + " / " + str(self.direccion_movimiento))
 
@@ -141,31 +197,28 @@ class Ventana_Custom(QtWidgets.QWidget):
     
     def loop_calcular_inercia(self):
         """Bucle de calculo de inercia"""
-        # TODO: Sistema de inercia
+
+        ultimo_x, ultimo_y = self.ultima_medida_posicion_inercia
+        self.ultima_medida_posicion_inercia = (self.x(), self.y())
 
         if self.agarrado:
 
-            ultimo_x, ultimo_y = self.ultima_medida_posicion_inercia
-            x_actual, y_actual = self.x(), self.y()
-
-            self.direccion_movimiento = math.atan2(x_actual - ultimo_x, y_actual - ultimo_y) # (radianes)
-            self.inercia = math.dist([ultimo_x, ultimo_y], [x_actual, y_actual])
-
-            self.ultima_medida_posicion_inercia = (x_actual, y_actual)
+            self.direccion_movimiento = math.atan2(self.x() - ultimo_x, self.y() - ultimo_y) # (radianes)
+            self.inercia = math.dist([ultimo_x, ultimo_y], [self.x(), self.y()])
 
         else: 
             if self.inercia > 0:
-                self.inercia -= round(self.inercia/10)
+                self.inercia -= self.inercia/10
             else:
                 self.inercia = 0
 
         #print("Inercia actual =" + str(self.inercia) + "  Direccion = " + str(self.direccion_movimiento))
 
- 
 
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    ventana = Ventana_Custom()
+    screen = app.primaryScreen()
+    ventana = Ventana_Custom(screen.size().width, screen.size().height)
     ventana.show()
     sys.exit(app.exec())
